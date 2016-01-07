6a62102e65d078dc7af06a9a2d65c486bdb819e8
awamahiu <aliwamahiu@gmail.com>
2016-01-07 04:22:18 -0500

    presentation functionality and more snaps

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 $(function () {
 
-    var currentPage = window.location.pathname;
-    console.log(currentPage);
-
-    
-// Moving main site to CMS to better manage portfolio...
-//    if (currentPage === "/work") {
-//        $(".workpage").addClass("active-page");
-//    } else {
-//        $(".homepage").addClass("active-page");
-//    }
-
-
     $(".navigation").animate({
         opacity: 1
     }, 500);
@@ -31,8 +19,35 @@ $(function () {
     $('#linkedin').tooltip();
     $('#resume').tooltip();
     $('#sendemail').tooltip();
-    
+    $('#snap-icon').tooltip();
+
     var date = new Date();
     $(".footer-date").append(date.getFullYear());
+    
+    var snapHover = function () {
+        
+    };
+    
+    snapHover();
+
+    // dynamic alts & titles
+    $(".snap").each(function(){
+        var $alt = $(this).find(".portfolio-snap-description").text();
+        var $title = $(this).find(".snap-title").text();
+ 
+        $(this).find("img").attr("alt", $alt);
+        $(this).find("img").attr("title", $title);
+    });
+    
+    // snaps/work presentation modals
+    $(".portfolio-snaps img").on("click", function () {
+        
+        $(".modal-body img").attr("src", $(this).attr("src"));
+        $(".snap-description").text($(this).attr("alt"));
+        $(".modal-title").text($(this).attr("title"));
+
+        $("#snapUp").modal({});
+        
+    });
 
 }); // Main function end
