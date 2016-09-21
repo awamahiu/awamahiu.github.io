1bb1cad7928bf1ba301e3e46f8ebfca7adf6d1e6
Ali Wamahiu <aliwamahiu@Alis-MacBook-Pro.local>
2016-09-21 05:05:59 -0400

    fixed favicon

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var routes = require('./routes/index');
 
 
@@ -19,30 +17,30 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 // view engine setup
 
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
+    defaultLayout: 'main',
+    partialsDir: ['views/partials/']
 }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
-// app.use(favicon(__dirname + '/favicon/favicon.ico'));
+app.use(favicon(__dirname + '/favicon/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8000, function () {
+    console.log('Example app listening on port 3000!');
 });
 
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -54,7 +52,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -66,7 +64,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
